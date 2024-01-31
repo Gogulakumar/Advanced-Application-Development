71a1ea9b6bfa12117de50efa5fafe5b838f28865
Gogulakumar <113664713+Gogulakumar@users.noreply.github.com>
2024-01-31 12:17:04 +0530

    Add files via upload

---
+++ Frontend/day 2/Day 2/src/Components/Signup.jsx
@@ -0,0 +1,83 @@
+// LoginPage.jsx
+
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './../Styles/LoginPage.css';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const validationErrors = {};
+    if (!username.trim()) {
+      validationErrors.username = 'Username is required';
+    }
+    if (!password.trim()) {
+      validationErrors.password = 'Password is required';
+    }
+
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
+    console.log('Logging in with:', { username, password });
+    // Perform login logic here
+
+    // Redirect to the home page
+  };
+
+  return (
+    <div>
+    <div style={{marginRight:'50px'}}>
+    <div className='base with-background'>
+      <div className="login-container">
+        <h2>SignUP</h2>
+        <form onSubmit={handleLogin}>
+          <div className="form-group">
+            <label htmlFor="username">Name:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            {errors.username && <div className="error">{errors.username}</div>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            {errors.username && <div className="error">{errors.username}</div>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {errors.password && <div className="error">{errors.password}</div>}
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        <p>
+          Do not have an account? <Link to="/" className="login-link">Login</Link>
+        </p>
+      </div>
+    </div>
+    </div>
+    </div>
+  );
+};
+
+export default LoginPage;
