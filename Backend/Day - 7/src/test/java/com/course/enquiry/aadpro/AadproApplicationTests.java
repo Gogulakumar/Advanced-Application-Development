df76b0faab5606087010a484651d36b8ae3e94ff
Gogulakumar <113664713+Gogulakumar@users.noreply.github.com>
2024-02-09 14:26:01 +0530

    Add files via upload

---
+++ Backend/Day - 7/src/test/java/com/course/enquiry/aadpro/AadproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.enquiry.aadpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
