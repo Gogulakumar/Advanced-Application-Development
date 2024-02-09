3a099b7b2ad6567d0df2ae8a4e9e339a01521a9c
Gogulakumar <113664713+Gogulakumar@users.noreply.github.com>
2024-02-09 14:29:48 +0530

    Add files via upload

---
+++ Backend/Day - 12/src/main/java/com/course/enquiry/aadpro/AadproApplication.java
@@ -0,0 +1,32 @@
+package com.course.enquiry.aadpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+
+
+
+@SpringBootApplication
+
+@OpenAPIDefinition(
+		info =@Info(
+				title = "Course Inquiry",
+				version = "1.1.2",
+				description = "Nanba Vanthuvittathu",
+				contact = @Contact(
+						name = "Author Name",
+						email = "Author Email"
+						)
+				)
+		)
+public class AadproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AadproApplication.class, args);
+	}
+
+}
